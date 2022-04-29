1e68b6c95f241b38e301e01c1170a4e6e9d96b80
Purachet Zheng <poorachad2545@gmail.com>
2022-04-30 00:43:53 +0700

    change server port to 5000 from 8080

--- server/src/main/java/sit/int221/oasipserver/dtos/EventDto.java
+++ server/src/main/java/sit/int221/oasipserver/dtos/EventDto.java
@@ -3,8 +3,11 @@ package sit.int221.oasipserver.dtos;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import sit.int221.oasipserver.entities.Event;
+import sit.int221.oasipserver.entities.Eventcategory;
 
 import java.time.Instant;
+import java.util.List;
 
 @Getter
 @Setter
@@ -17,4 +20,7 @@ public class EventDto {
     private Integer eventDuration;
     private Instant eventStartTime;
     private String eventNotes;
+//    private String eventCategoryEventCategoryDescription;
+//    private List<Event> eventCategoryEvents;
+//    private Eventcategory eventCategory;
 }
--- server/src/main/java/sit/int221/oasipserver/entities/Eventcategory.java
+++ server/src/main/java/sit/int221/oasipserver/entities/Eventcategory.java
@@ -1,5 +1,6 @@
 package sit.int221.oasipserver.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -20,6 +21,7 @@ public class Eventcategory {
     @Column(name = "eventDuration", nullable = false)
     private Integer eventDuration;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "eventCategory")
     private Set<Event> events = new LinkedHashSet<>();
 
