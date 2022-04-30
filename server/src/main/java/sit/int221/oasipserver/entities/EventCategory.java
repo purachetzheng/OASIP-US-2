cf9c2350f8a2da31b920a031dafa4e135c3bbeee
Purachet Zheng <poorachad2545@gmail.com>
2022-04-30 23:56:31 +0700

    .

--- server/src/main/java/sit/int221/oasipserver/entities/EventCategory.java
+++ server/src/main/java/sit/int221/oasipserver/entities/EventCategory.java
@@ -1,7 +1,29 @@
 package sit.int221.oasipserver.entities;
 
-import javax.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.*;
+import java.util.LinkedHashSet;
+import java.util.Set;
+@Setter @Getter
 @Entity
-public class EventCategory {
+@Table(name = "EventCategories")
+public class Eventcategory {
+    @Id
+    @Column(name = "eventCategoryName", nullable = false, length = 100)
+    private String id;
+
+    @Column(name = "eventCategoryDescription", length = 500)
+    private String eventCategoryDescription;
+
+    @Column(name = "eventDuration", nullable = false)
+    private Integer eventDuration;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "eventCategory")
+    private Set<Event> events = new LinkedHashSet<>();
+
+
 }
