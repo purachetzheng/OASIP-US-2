079a64d22d32db5bffedd4cc215ef03be3d3e12d
Phumin63130500094 <chumphu.phumin@gmail.com>
2022-05-02 01:29:14 +0700

    addCors

--- server/src/main/java/sit/int221/oasipserver/ApplicationConfig.java
+++ server/src/main/java/sit/int221/oasipserver/ApplicationConfig.java
@@ -3,15 +3,17 @@ package sit.int221.oasipserver;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import sit.int221.oasipserver.services.ListMapper;
 
 @Configuration
-public class ApplicationConfig {
+public class ApplicationConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
     }
-
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
