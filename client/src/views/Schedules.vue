ec7b3a52f8f9e33074a117bea46d759a03bf5c60
Purachet Zheng <poorachad2545@gmail.com>
2022-05-08 05:33:41 +0700

    remove event

--- client/src/views/Schedules.vue
+++ client/src/views/Schedules.vue
@@ -11,7 +11,8 @@ import { useMouse } from '../js/mouse'
 import { useRoute, useRouter } from 'vue-router'
 
 import { events, eventCategories, middleFetch } from '../js/variable'
-dayjs.extend(localizedFormat)
+import CarbonTrashCan from '../components/icons/CarbonTrashCan.vue'
+// dayjs.extend(localizedFormat)
 
 //use router
 // const { params } = useRoute()
@@ -25,17 +26,16 @@ onBeforeMount(async () => {
     await middleFetch.getEventCategoriesNull()
 })
 
-const rId = ref(0)
 let mouseOnRow = ref(null)
-const getDate = (dateTime) => dayjs(dateTime).format('LL')
-const getTime = (dateTime) => dayjs(dateTime).format('HH:mm')
-// const getTime = (dateTime) => dayjs(dateTime).format('LT')
 
 const goTo = (pageName, param = null) => router.push({ name: pageName, params: param ? param : '' })
 const viewDetail = (id) => {
     goTo('EventDetail', { eventId: id })
 }
 
+const removeEvent = async (id) => {
+    events.value = await zFetch.remove('http://intproj21.sit.kmutt.ac.th/us2/api/events', id) ? events.value.filter((event) => event.id !== id) : ''
+}
 </script>
 
 <template>
@@ -48,32 +48,39 @@ const viewDetail = (id) => {
         <span class="text-gray-100 text-lg" v-show="events.length === 0">No Scheduled Events</span>
         <div class="grid xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-1 gap-x-4 gap-y-2">
             <!-- bg-neutral-800 hover:bg-neutral-600 -->
-            <div class="flex bg-neutral-800 hover:bg-neutral-600 cursor-pointer rounded-lg"
-                v-for="(event, index) in events" @click="viewDetail(event.id)" @mouseenter="mouseOnRow = index"
+            <div class="relative flex bg-neutral-800 hover:bg-neutral-600 cursor-pointer rounded-lg"
+                v-for="(event, index) in events" @mouseenter="mouseOnRow = index"
                 @mouseleave="mouseOnRow = null">
                 <!-- <div class=" inset-y-0 left-0 p-0.5 rounded-l-lg bg-red-600" aria-hidden="true"></div> -->
                     <!-- <div class="pl-4 py-4 rounded-l-lg lg:w-3/12 md:w-2/12  block h-full shadow-inner min-w-[25%]">
                         <div class="text-base text-gray-300 tracking-wide ">{{ dayjs(event.eventStartTime).format('MMM YY') }}</div>
                         <div class="font-semibold text-4xl text-gray-100">{{ dayjs(event.eventStartTime).format('D') }}</div>
                     </div> -->
-                <span class="bg-cyan-600 w-2 rounded-l-lg"></span>
-                <div class="rounded-lg lg:w-3/12 py-2 sm:px-4 block h-full shadow-inner">
-                    <div class="text-center">
-                        <div class="text-white font-bold text-4xl">{{ dayjs(event.eventStartTime).format('D') }}</div>
-                        <div class="text-white font-normal text-2xl tracking-wider">{{ dayjs(event.eventStartTime).format('MMM') }} </div>
-                        <div class="text-white font-normal text-xl tracking-wider">2022</div>
+                <div class="h-full w-full flex" @click="viewDetail(event.id)">
+                        <span class="bg-cyan-600 w-2 rounded-l-lg"></span>
+                    <div class="rounded-lg lg:w-3/12 py-2 sm:px-4 block h-full shadow-inner">
+                        <div class="text-center">
+                            <div class="text-white font-bold text-4xl">{{ dayjs(event.eventStartTime).format('D') }}</div>
+                            <div class="text-white font-normal text-2xl tracking-wider">{{ dayjs(event.eventStartTime).format('MMM') }} </div>
+                            <div class="text-white font-normal text-xl tracking-wider">2022</div>
+                        </div>
+                    </div>
+                    <div class="bg-slate-200 w-0.5 my-5"></div>
+                    <div class="w-full py-4 px-4 flex flex-col justify-center ">
+                        <div class="font-bold text-xl text-gray-200">{{ event.bookingName }}</div>
+                        <div class="font-semibold text-gray-300">{{ event.eventCategoryId }}</div>
+                        <div class="text-gray-400">{{ dayjs(event.eventStartTime).format('LT') }} ({{
+                                event.eventDuration
+                        }} minutes )
+                        </div>
                     </div>
                 </div>
-                <div class="bg-slate-200 w-0.5 my-5"></div>
-                <div class="w-full py-4 px-4 flex flex-col justify-center ">
-                    <div class="font-bold text-xl text-gray-200">{{ event.bookingName }}</div>
-                    <div class="font-semibold text-gray-300">{{ event.eventCategoryId }}</div>
-                    <div class="text-gray-400">{{ dayjs(event.eventStartTime).format('LT') }} ({{
-                            event.eventDuration
-                    }} minutes )
+                
+                <div class="w-2/12 p-2 flex flex-col justify-center items-center">
+                    <div class="" @click="removeEvent(event.id)">
+                        <CarbonTrashCan class="text-3xl text-gray-200 text-center"/>
                     </div>
                 </div>
-                <!-- <div class="w-2/12 p-2">Footer</div> -->
             </div>
 
         </div>
--- client/vite.config.js
+++ client/vite.config.js
@@ -4,17 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://ip21us2.sit.kmutt.ac.th:8080',
-        changeOrigin: true,
-        secure: false,      
-        ws: true,
-        // rewrite:(path) => path.replace(/^\/api/, '')
-      }
-    }
-    
-  }
+  base: 'http://intproj21.sit.kmutt.ac.th/us2/'
 })
 
