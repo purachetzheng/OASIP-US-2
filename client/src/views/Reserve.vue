815264f666364808c6e99a36aec5fe7d8dacc7eb
Purachet Zheng <poorachad2545@gmail.com>
2022-05-08 05:06:51 +0700

    fix submit

--- client/src/views/Reserve.vue
+++ client/src/views/Reserve.vue
@@ -23,7 +23,7 @@ let startTime = ref(null)
 const submit = async () => {
     createEvent.value.eventStartTime = dayjs(startDate.value + startTime.value).toJSON()
     const addedEvent = await zFetch.post('http://ip21us2.sit.kmutt.ac.th:8080/api/events', createEvent.value)
-    addedEvent ? events.value.push(addedEvent) : '';
+    addedEvent ? events.value.push(addedEvent) : ''
 }
 
 const goBack = () => router.go(-1)
@@ -37,13 +37,15 @@ const selectCatagory = (e) => selectedCatagory.value = eventCategories.value.fin
  
 <template>
     <main class="h-screen w-screen screen-p-1 flex justify-center overflow-y-scroll items-center">
-        <form class="flex flex-col gap-2 h-full w-1/2 overflow-y-scroll p-8 bg-white rounded-xl shadow-lg">
+        <form 
+            class="flex flex-col gap-2 h-full w-1/2 overflow-y-scroll p-8 bg-white rounded-xl shadow-lg"
+            @submit="submit">
             <span class="text-h-1 text-black mb-2">Reserve</span>
             <div class="flex gap-4">
                 <div class="w-full flex flex-col gap-0.5 ">
                     <div class="text-xs">Select Catagory</div>
-                    <select class="form-1 font-medium" v-model="createEvent.eventCategoryId" required @change="selectCatagory">
-                        <option v-for="eventC in eventCategories" :value="eventC.id">{{ eventC.id }}</option>
+                    <select class="form-1 font-medium" v-model="createEvent.eventCategoryId" required @change="selectCatagory" name="eventCategoryId">
+                        <option v-for="eventC in eventCategories" :value="eventC.id">{{ eventC.eventCategoryName }}</option>
                     </select>
                 </div>
                 <div class="flex flex-col gap-0.5">
@@ -63,18 +65,18 @@ const selectCatagory = (e) => selectedCatagory.value = eventCategories.value.fin
             </div>
             <div class="flex flex-col gap-0.5">
                 <div class="text-xs">Name</div>
-                <input type="text" required class="form-1 font-medium" placeholder="Name" name="test"
+                <input type="text" required class="form-1 font-medium" placeholder="Name" name="bookingName"
                     v-model="createEvent.bookingName">
             </div>
             <div class="flex flex-col gap-0.5">
                 <div class="text-xs">Email</div>
-                <input type="email" required class="form-1 font-medium" placeholder="Email"
+                <input type="email" required class="form-1 font-medium" placeholder="Email" name="bookingEmail"
                     v-model="createEvent.bookingEmail">
             </div>
             <div class="flex gap-5">
                 <div class="w-full flex flex-col gap-1">
                     <div class="text-xs">Note:</div>
-                    <textarea class=" form-1 font-medium" rows="5" placeholder="Note"
+                    <textarea class=" form-1 font-medium" rows="5" placeholder="Note" name="evnetNotes"
                         v-model="createEvent.evnetNotes"></textarea>
                 </div>
             </div>
@@ -82,8 +84,9 @@ const selectCatagory = (e) => selectedCatagory.value = eventCategories.value.fin
             <div class="flex gap-6 justify-end mt-4">
                 <button @click="goBack"
                     class="btn-1 bg-white hover:bg-gray-100 text-gray-600">Cancel</button>
-                <button @click="submit"
-                    class="btn-1 bg-blue-600 hover:bg-blue-700 text-white">Submit</button>
+                    <!-- ยังไม่ได้ทำเช็ค -->
+                <button type="submit"
+                    class="btn-1 bg-blue-600 hover:bg-blue-700 text-white" value="Submit">Submit</button>
             </div>
         </form>
     </main>
