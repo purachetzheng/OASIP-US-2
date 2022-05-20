0dba1f6de0f13269105063df3066853b6909d3fb
Purachet Zheng <poorachad2545@gmail.com>
2022-05-20 20:03:43 +0700

    test env

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,8 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'http://intproj21.sit.kmutt.ac.th/us2/'
-  // base: '/us2/'
+  // base: 'intproj21.sit.kmutt.ac.th/us2/'
+  base: '/us2/'
   // base: process.env.VITE_BASE_URL
 })
 
