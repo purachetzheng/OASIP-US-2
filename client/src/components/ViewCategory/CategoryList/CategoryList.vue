d49304a2173c6c745ab5215679c85fcf27bed68e
Phumin63130500094 <chumphu.phumin@gmail.com>
2022-05-23 10:20:28 +0700

    ! fix text style in category-list, select-category

--- client/src/components/ViewCategory/CategoryList/CategoryList.vue
+++ client/src/components/ViewCategory/CategoryList/CategoryList.vue
@@ -25,21 +25,21 @@ const color = [
         'grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1', 'gap-x-4 gap-y-4']">
         <div v-for="category in categories" :key="category.id"
             @click="$emit('emitViewDetail', category.id)"
-            :class="['h-52 justify-between', 'flex flex-col', 'px-12 py-8 border', 'hover:text-gray-50' , 
+            :class="['h-52 justify-between', 'flex flex-col', 'px-8 py-8 border', 'hover:text-gray-50' , 
                 'rounded-lg shadow-md cursor-pointer ', 
-                'hover:rose-yellow-1', 'transition'
+                'hover:rose-yellow-1', 'transition', 'bg-white'
                 ]">
             <div class="flex flex-col gap-2">
                 <div class="flex items-center gap-1.5">
                     <CategoryBox />
-                    <p class="text-lg font-semibold truncate ">{{ category.eventCategoryName }} </p>
+                    <p class="text-lg font-semibold truncate">{{ category.eventCategoryName }} </p>
                 </div>
                 <div class="overflow-auto h-18">
                     <span class="text-sm  overflow-auto" >{{ category.eventCategoryDescription }}</span>
                 </div>
             </div>
-            <div class="flex  justify-between ">
-                <div class="flex  items-center gap-1">
+            <div class="flex justify-between">
+                <div class="flex items-center gap-1">
                     <IconHourglass class="w-5 h-5" />
                     <span class="text-sm font-medium">Duration: {{ category.eventDuration }} min</span>
                 </div>
