7c5418f23bc6c1db4ec7ef4e04ac2008368ff139
Phumin63130500094 <chumphu.phumin@gmail.com>
2022-05-23 19:41:33 +0700

    update home

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -13,18 +13,20 @@ const goTo = (pageName, param = null) => router.push({ name: pageName, params: p
 <template>
     <div class="flex h-full">
         <div class="flex h-full w-full cursor-default">
-            <div class="flex flex-col h-full w-full justify-center items-center gap-6">
-                <div class="flex flex-col text-3xl" @mouseenter="mouseNow = 'logo'"
+            <div class="flex flex-col h-full w-full justify-center items-center gap-6 bg-gradient-to-r
+    from-blue-500
+    to-purple-500">
+                <div class="flex flex-col text-3xl text-white" @mouseenter="mouseNow = 'logo'"
                     @mouseleave="mouseNow = null">
-                    <h2 class="text-4xl font-semibold text-gray-700 dark:text-gray-200">OASIP-US2</h2>
-                    <div class="font-thin dark:text-gray-200">
+                    <div class="font-semibold dark:text-gray-200">
                         Reservation Web Application
                     </div>
+                    <h2 class="font-thin dark:text-gray-200">OASIP-US2</h2>
                     <div class="flex text-lg gap-2 mt-2">
                         <button @click="goTo('Schedules')"
-                            class="p-1 px-4 rounded bg-cyan-600 hover:bg-cyan-700 text-white transition">Schedules</button>
+                            class="p-1 px-4 rounded bg-white hover:bg-gray-900 hover:text-white text-gray-900 transition">Schedules</button>
                         <button @click="goTo('Reserve')"
-                            class="p-1 px-4 rounded bg-cyan-600 hover:bg-cyan-700 text-white transition">Reserve</button>
+                            class="p-1 px-4 rounded bg-white hover:bg-gray-900 hover:text-white text-gray-900 transition">Reserve</button>
                     </div>
                 </div>
             </div>
