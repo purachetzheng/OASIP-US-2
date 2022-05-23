e4a03ec71d9b9ac2924bb0617e741bd971f3b5c9
Purachet Zheng <poorachad2545@gmail.com>
2022-05-24 03:19:12 +0700

    fix

--- server/src/main/java/sit/int221/oasipserver/services/EventService.java
+++ server/src/main/java/sit/int221/oasipserver/services/EventService.java
@@ -1,9 +1,6 @@
 package sit.int221.oasipserver.services;
 
-<<<<<<< HEAD
 
-=======
->>>>>>> dev
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
